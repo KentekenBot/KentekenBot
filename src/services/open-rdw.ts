ad5025a20d7eae107ec727182c3def76fdf05ce5
Daan Peters <daan@lyfter.nl>
2022-05-18 22:26:42 +0200

    Refactor: Add missing token header

--- src/services/open-rdw.ts
+++ src/services/open-rdw.ts
@@ -1,13 +1,26 @@
 import axios from 'axios';
+import { Settings } from './settings';
+import { AvailableSettings } from '../enums/available-settings';
 
 export class OpenRdw {
     public async getVehicleInfo(licence: string): Promise<Record<string, unknown> | undefined> {
-        const response = await axios.get(`https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=${licence}`);
+        return await this.makeRequest(`m9d7-ebf2.json?kenteken=${licence}`);
+    }
+    public async getFuelInfo(licence: string): Promise<Record<string, unknown>> {
+        return await this.makeRequest(`8ys7-d773.json?kenteken=${licence}`);
+    }
+
+    private async makeRequest(endpoint: string): Promise<Record<string, unknown>> {
+        const response = await axios.get(`https://opendata.rdw.nl/resource/${endpoint}`, this.getRequestOptions());
+
         return response.data[0];
     }
 
-    public async getFuelInfo(licence: string): Promise<Record<string, unknown>> {
-        const response = await axios.get(`https://opendata.rdw.nl/resource/8ys7-d773.json?kenteken=${licence}`);
-        return response.data[0];
+    private getRequestOptions() {
+        return {
+            headers: {
+                'X-App-Token': Settings.get(AvailableSettings.OPEN_DATA_TOKEN),
+            },
+        };
     }
 }
