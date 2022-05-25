3b2b2d206bf36ef56935b71816c36f6fa7fb2fbd
Kevin van Hulst <kevin.van.hulst@gmail.com>
2022-05-25 11:48:17 +0200

    add status command

---
+++ src/commands/status.ts
@@ -0,0 +1,17 @@
+import { MessageEmbed } from 'discord.js';
+import { ICommand } from '../interfaces/command';
+import { BaseCommand } from './base-command';
+import moment from 'moment';
+import { uptime } from 'os';
+
+export class Status extends BaseCommand implements ICommand {
+    public handle(): void {
+        const response = new MessageEmbed().addFields(
+            { name: 'Bot uptime', value: moment.duration(this.client.uptime).humanize() },
+            { name: 'Server uptime', value: moment.duration(uptime(), 'seconds').humanize() },
+            { name: 'Guild count', value: this.client.guilds.cache.size.toString() }
+        );
+
+        this.reply({ embeds: [response] });
+    }
+}
