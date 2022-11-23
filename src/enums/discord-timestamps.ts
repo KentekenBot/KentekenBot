115fd06e02612c54db2f3ab11067695acefc768c
Daan Peters <daan@lyfter.nl>
2022-11-23 22:18:58 +0100

    Add timestamps enum and improve construction date

---
+++ src/enums/discord-timestamps.ts
@@ -0,0 +1,10 @@
+export enum DiscordTimestamps {
+    DEFAULT = '',
+    SHORT_TIME = 't',
+    LONG_TIME = 'T',
+    SHORT_DATE = 'd',
+    LONG_DATE = 'D',
+    SHORT_DATE_TIME = 'f',
+    LONG_DATE_TIME = 'F',
+    RELATIVE = 'R',
+}
--- src/util/date-time.ts
+++ src/util/date-time.ts
@@ -1,6 +1,8 @@
+import { DiscordTimestamps } from '../enums/discord-timestamps';
+
 export class DateTime {
-    public static getDiscordTimestamp(timestamp: number): string {
-        return `<t:${this.millisecondsToSeconds(timestamp)}:R>`;
+    public static getDiscordTimestamp(timestamp: number, type = DiscordTimestamps.RELATIVE): string {
+        return `<t:${this.millisecondsToSeconds(timestamp)}:${type}>`;
     }
 
     public static millisecondsToSeconds(timestamp: number): number {
