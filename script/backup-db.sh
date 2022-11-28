3801615fe10e883b1af3c150397619a27ce7693a
Daan Peters <daan@lyfter.nl>
2022-11-28 19:20:46 +0100

    Fix syntax error in db backup script

--- script/backup-db.sh
+++ script/backup-db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3 cp ../kentekenbot.db s3://kentekenbot-backups/$(date +"%Y-%m-%d_%H%M%S")_kentekenbot.db --profile=kentekenbot\
+aws s3 cp ../kentekenbot.db s3://kentekenbot-backups/$(date +"%Y-%m-%d_%H%M%S")_kentekenbot.db --profile=kentekenbot
