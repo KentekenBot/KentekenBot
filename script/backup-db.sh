5001bb799b3e37444ee5b18f12b7ea81d1db5f49
Daan Peters <daan@lyfter.nl>
2022-11-29 09:18:16 +0100

    Use full path to aws cli

--- script/backup-db.sh
+++ script/backup-db.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3 cp ../kentekenbot.db s3://kentekenbot-backups/$(date +"%Y-%m-%d_%H%M%S")_kentekenbot.db --profile=kentekenbot\
+/usr/local/bin/aws s3 cp ../kentekenbot.db s3://kentekenbot-backups/$(date +"%Y-%m-%d_%H%M%S")_kentekenbot.db --profile=kentekenbot\
