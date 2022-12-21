52fef1860ca8f37182f79e1c3de3bbcf534dadb0
Daan Peters <daan@lyfter.nl>
2022-12-21 19:44:53 +0100

    Cleanup

--- src/commands/base-command.ts
+++ src/commands/base-command.ts
@@ -15,15 +15,6 @@ export abstract class BaseCommand {
         return (this.interaction?.options.get(name)?.value as unknown as T) || undefined;
     }
 
-    protected getArguments(): string[] {
-        // console.log(this.interaction.options.get());
-        return [];
-        // const data = this.interaction.content.replace(Settings.get(AvailableSettings.COMMAND_PREFIX), '').split(' ');
-        // data.shift();
-        //
-        // return data;
-    }
-
     protected reply(options: string | MessagePayload | InteractionReplyOptions): void {
         this.interaction?.reply(options);
     }
