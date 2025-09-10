97eb8b8d4eaa2864f85ce453cbfb6ec266e74c91
Kevin van Hulst <kevin.van.hulst@mailtopay.nl>
2025-09-10 16:29:21 +0200

    Revert "add apdie support"

--- src/util/date-time.ts
+++ src/util/date-time.ts
@@ -2,9 +2,6 @@ import { DiscordTimestamps } from '../enums/discord-timestamps';
 
 export class DateTime {
     public static getDiscordTimestamp(timestamp: number, type = DiscordTimestamps.DEFAULT): string {
-        if (!timestamp) {
-            return 'Onbekende datum';
-        }
         return `<t:${this.millisecondsToSeconds(timestamp)}${type ? ':' : ''}${type}>`;
     }
 
